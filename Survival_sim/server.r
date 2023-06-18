b33aa1e75a31c59ab0a19c59900ada2e5130257c
MVCowley <mvc28@bath.ac.uk>
2023-06-18 07:26:51 +0100

    Renamed CA and added license grant to files

--- Survival_sim/server.r
+++ Survival_sim/server.r
@@ -1,3 +1,17 @@
+# This file is part of a set of shiny applications for the analysis of in silico
+# FSHD muscle fibres.
+# Copyright © 2023 C. R. S. Banerji
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <https://www.gnu.org/licenses/>.
+
 load("files_for_KM_plot_shiny_not_refined.rd")
 library(shiny)
 library("tidyverse")
